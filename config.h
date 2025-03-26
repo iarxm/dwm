ab0bc6e2b7b3664cc24d1414845515dfce19757f
Iarom Madden <mail@iarom.org>
2025-03-26 15:15:45 +0000

    dmenu

--- config.h
+++ config.h
@@ -21,11 +21,11 @@ static const char *tags[] = {
     "xoo", /* 9*/
     "xmj", /* 10*/
     "xml", /* 11*/
-    "xmk",
-    "xmr",
-    "xrd",
+    "xmk", /* 12*/
+    "xmr", /* 13*/
+    "xrd", /* 14*/
     "xau", /* 15*/
-    "xsx",
+    "xsx", /* 16*/
 };
 
 static const Rule rules[] = {
@@ -60,17 +60,17 @@ static const Layout layouts[] = {
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define shcmd(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, null } }
 
-static const char dmenudef[] = { "-nb 'black' -nf '#efef8f' -sb 'black' -sf 'white'" };
+/*static const char *dmenudef[] = { "-nb", "black", "-nf", "#efef8f", "-sb", "black", "-sf", "white" };*/
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-b", "-fn", dmenufont, "-nb", col_blk1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
-/*static const char *dmpass[]  = { "passmenu", "-b", "-l 20", dmenudef, null };*/
+static const char *dmpass[]  = { "passmenu", "-b", "-l 20", "-nb", "black", "-nf", "#efef8f", "-sb", "black", "-sf", "white", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKY1,                       XK_p,      spawn,          {.v = dmenucmd } },
-    /*{ MODKY1,                       XK_p,      spawn,          {.v = dmpass } },*/
+    { MODKY1,                       XK_p,      spawn,          {.v = dmpass } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_q,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
@@ -84,6 +84,7 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
+	{ MODKY1,                       XK_Tab,    view,           {0} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
